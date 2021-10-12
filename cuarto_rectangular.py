b05190230e09635a81055a055404769a6f9d71ac
RODRIGO GARCIA <90258887+Akaza-Hakuji@users.noreply.github.com>
2021-10-12 12:48:07 +0000

    Empezamos a hacer el código de el cuarto_rectangular.py

---
+++ cuarto_rectangular.py
@@ -0,0 +1,9 @@
+# este programa calcula el area de un cuarto rectangular y muestra el resultado en el sistema métrico (metros cuadrados) y en sistema imperial (pies cuadrados)
+# 1. Inicio
+# 2. Definir ancho, largo, área_metros, área_pies, factor_conversión.
+# 3.  Hacer factor_conversión = 10.764
+factor_conversion = 10.764
+# 4. Imprimir "¿Cual es el largo del cuarto en metros?"
+print ("¿Cual es el largo del cuarto en metros?")
+# 5. Hacer largo = entrada del usuario
+largo = input() # convertir a número
