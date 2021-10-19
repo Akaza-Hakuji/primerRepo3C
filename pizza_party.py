4451eb8c89dd55178617312c61da24669bddb739
Akaza-Hakuji <90258887+Akaza-Hakuji@users.noreply.github.com>
2021-10-19 07:43:55 -0500

    Create pizza_party.py

---
+++ pizza_party.py
@@ -0,0 +1,15 @@
+# 1. Inicio.
+# 2. Definir personas, pizzas, rebanadas. 
+# 3. Imprimir "¿Cuántas personas vendrán?" 
+# 4. Hacer personas = entrada del usuario.
+# 5. Imprimir "¿Cuántas pizzas tienes?"
+# 6. Hacer pizzas = entrada del usuario.
+# 7. Imprimir "¿De cuántas rebanadas?"
+# 8. Hacer rebanadas = entrada del usuario
+# 9. Hacer número_de_rebanadas = pizzas * rebanadas.
+# 10. Hacer rebanadas_por_persona = número_de_rebanadas // personas
+# 11. Hacer sobrante = número_de_rebanadas % personas
+# 11. Imprimir "personas, personas y, pizzas, pizzas"
+# 12. Imprimir "Cada persona podrá comer, rebanadas_por_persona, rebanadas de pizza.
+# 13. Imprimir "Sobrarán, sobrante, rebanadas"
+# 14. Fin.
